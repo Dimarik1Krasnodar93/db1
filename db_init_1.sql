a1400a70b05473dac52c3d8e351d92df623b311e
Dmitry Ostapov <87864463+Dimarik1Krasnodar93@users.noreply.github.com>
2024-01-06 21:07:07 +0300

    Update db_init_1.sql - distinct on

--- db_init_1.sql
+++ db_init_1.sql
@@ -1,16 +1,12 @@
-SELECT 
+SELECT DISTINCT ON (client.client_id)
 client.client_id client_id,
 client.first_name first_name,
 client.last_name last_name,
 client.phone phone,
 client.email email,
-MAX(transaction.amount) amount,
-MAX(transaction.created_on) created_on
+transaction.created_on created_on,
+transaction.amount amount
 FROM client 
 LEFT JOIN 
 transaction ON client.client_id =  transaction.client_id
-GROUP BY client.client_id, 
-client.first_name,
-client.last_name,
-client.phone,
-client.email
+ORDER BY client_id, amount DESC, created_on DESC
