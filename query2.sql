714b653b62f494650538950e1cf36f1cc1e82f90
Dmitry Ostapov <87864463+Dimarik1Krasnodar93@users.noreply.github.com>
2024-01-06 11:36:51 +0300

    Create query2.sql

---
+++ query2.sql
@@ -0,0 +1,8 @@
+SELECT 
+transaction.created_on date_trunc,
+transaction.client_id client_id,
+SUM(CASE WHEN transaction.amount > 0 THEN transaction.amount ELSE 0 END) debit,
+SUM(CASE WHEN transaction.amount < 0 THEN transaction.amount ELSE 0 END) credit
+FROM transaction
+GROUP BY date_trunc, client_id
+ORDER BY date_trunc
